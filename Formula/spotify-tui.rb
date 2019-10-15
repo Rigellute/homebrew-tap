61216ee1231a7f46c29c0f570e992de72b980729
Rigellute <rigellute@gmail.com>
2019-10-15 13:49:43 +0100

    Release version 0.6.2

--- Formula/spotify-tui.rb
+++ Formula/spotify-tui.rb
@@ -1,5 +1,5 @@
 class SpotifyTui < Formula
-  version "v0.6.1"
+  version "v0.6.2"
   desc "A terminal user interface for Spotify"
   homepage "https://github.com/Rigellute/spotify-tui"
 
@@ -7,7 +7,7 @@ class SpotifyTui < Formula
 
   if OS.mac?
     url "https://github.com/Rigellute/spotify-tui/releases/download/#{version}/spotify-tui-#{version}.tar.gz"
-    sha256 "3be5317c6d0e6bafed7fcadf96c077997bc307257aae2d90f3b507da68bfa620"
+    sha256 "11fdcd06018e47d4db1296f1cf0e02a2bbb1b915f364007a6f7208063ec41d5a"
   end
 
   def install
