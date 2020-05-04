31801d756504c1edb8e99baab7540e2c4b6a7d3f
Alexander Keliris <rigellute@gmail.com>
2020-05-04 14:08:03 +0100

    Release v0.19.0

--- Formula/spotify-tui.rb
+++ Formula/spotify-tui.rb
@@ -1,5 +1,5 @@
 class SpotifyTui < Formula
-  version "v0.18.0"
+  version "v0.19.0"
   desc "A terminal user interface for Spotify"
   homepage "https://github.com/Rigellute/spotify-tui"
 
@@ -7,10 +7,10 @@ class SpotifyTui < Formula
 
   if OS.mac?
     url "https://github.com/Rigellute/spotify-tui/releases/download/#{version}/spotify-tui-macos.tar.gz"
-    sha256 "2a8109bf45a353f6f437e75bb1a5b97b22d159f60fca0fa948761d38c4cd8165"
+    sha256 "d991891a88f3ee08cc60fef31afd0f4a0adb5f3c8e6433bd7cceb3eba2562ad3"
   elsif OS.linux?
     url "https://github.com/Rigellute/spotify-tui/releases/download/#{version}/spotify-tui-linux.tar.gz"
-    sha256 "b8ab25d8683ffcc5a578c44a8da88f1835fcfe9963312a406719c80aa8e78fa9"
+    sha256 "b5a40090ec1a3b3013214e9ac389775f0a07ae8c90cbac6a9b2869f99ad873c3"
   end
 
   def install
